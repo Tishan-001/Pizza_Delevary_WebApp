74f422ec9d90c3288ce9c31e153fd747b3658b74
Tishan-001 <133506972+Tishan-001@users.noreply.github.com>
2023-09-05 14:03:46 +0530

    hero section add

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import classes from './home.module.css'
+import Hero from "../hero/Hero";
 
 const Home = () => {
     return(
-        <div>Home</div>
+        <div className={classes.container}>
+            <div className={classes.wrapper}>
+                <Hero />
+            </div>
+            
+        </div>
     )
 }
 
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom'
 import classes from './navbar.module.css'
 import {AiOutlineUser, AiOutlineShoppingCart} from 'react-icons/ai'
 
 const Navbar = () => {
+
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset === 0 ? false: true)
+        return () => (window.onscroll = null)
+    }
     return(
-        <div className={classes.cantainer}>
+        <div className={`${classes.cantainer} ${isScrolled && classes.scrolled}`}>
             <div className={classes.wrapper}>
                 <div className={classes.left}>
                     <Link to='/' className={classes.title}>
